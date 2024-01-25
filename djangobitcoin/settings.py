93589957a9d1866cf48c7f3e810cf0fb586bcc15
Danny Nguyen <nguyenduongpa@gmail.com>
2024-01-24 18:22:41 -0800

    postgres database connected

--- djangobitcoin/settings.py
+++ djangobitcoin/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangobitcoin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bitcoinwallet',
+        'USER': 'postgres',
+        'PASSWORD': 'DannyDev89',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
