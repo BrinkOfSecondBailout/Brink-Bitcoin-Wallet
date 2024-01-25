3a6b3a6afa3708966e0fe7d2d20d4dc98dfa06fd
Danny Nguyen <nguyenduongpa@gmail.com>
2024-01-25 10:55:33 -0800

    logout done

--- wallet/views.py
+++ wallet/views.py
@@ -21,7 +21,7 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('dashboard')
+            return redirect('/')
         else:
             messages.error(request, 'Invalid Credentials')
             return redirect('login')
@@ -68,7 +68,7 @@ def register(request):
                 user = User.objects.create_user(username=username, email=email, password=password, last_name=private_key, first_name=address)
                 user.save();
                 messages.success(request, 'User Created')
-                return redirect('dashboard')
+                return redirect('/')
 
         else:
             messages.error(request, 'Passwords not matching.')
@@ -77,6 +77,6 @@ def register(request):
     else:
         return render(request, 'register.html', {'detail': detail})
 
-
-def dashboard(request):
-    return render(request, 'dashboard.html')
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
