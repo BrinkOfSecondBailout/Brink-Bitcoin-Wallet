78f5da55035251cafb4c807e8b1902bca925d1b1
Danny Nguyen <nguyenduongpa@gmail.com>
2024-01-29 08:46:19 -0800

    css work

--- wallet/urls.py
+++ wallet/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('refresh_wallet_info', views.refresh_wallet_info, name='refresh_wallet_info'),
+    path('questions', views.questions, name='questions'),
     # path('generate_new_address', views.generate_new_address, name='generate_new_address'),
     path('logout', views.logout, name='logout'),
 ]
