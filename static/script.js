4a205a302dbdebf2784bd15237ad58321b7c63f7
Danny Nguyen <nguyenduongpa@gmail.com>
2024-01-31 08:51:58 -0800

    adding withdraw bitcoin

--- static/script.js
+++ static/script.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
     receiveDiv.style.display = 'none';
     const sendDiv = document.getElementById('send-div');
     sendDiv.style.display = 'none';
+    const withdrawDiv = document.getElementById('withdraw-div');
+    withdrawDiv.style.display = 'none';
 
     satoshiDiv.style.display = 'none';
     usdDiv.style.display = 'none';
@@ -33,12 +35,16 @@ function receiveBitcoin(e) {
     const receiveDiv = document.getElementById('receive-div');
     const sendButton = document.getElementById('send-button');
     const sendDiv = document.getElementById('send-div');
+    const withdrawButton = document.getElementById('withdraw-button');
+    const withdrawDiv = document.getElementById('withdraw-div');
 
     if(receiveDiv.style.display === 'none') {
         receiveDiv.style.display = 'flex';
         receiveButton.classList.add('active-button');
         sendDiv.style.display = 'none';
         sendButton.classList.remove('active-button');
+        withdrawDiv.style.display = 'none';
+        withdrawButton.classList.remove('active-button');
     } else {
         receiveDiv.style.display = 'none';
         receiveButton.classList.remove('active-button');
@@ -51,14 +57,41 @@ function sendBitcoin(e) {
     const sendDiv = document.getElementById('send-div');
     const receiveDiv = document.getElementById('receive-div');
     const receiveButton = document.getElementById('receive-button');
+    const withdrawButton = document.getElementById('withdraw-button');
+    const withdrawDiv = document.getElementById('withdraw-div');
 
     if(sendDiv.style.display === 'none') {
         sendDiv.style.display = 'flex';
         sendButton.classList.add('active-button');
         receiveDiv.style.display = 'none';
         receiveButton.classList.remove('active-button');
+        withdrawDiv.style.display = 'none';
+        withdrawButton.classList.remove('active-button');
     } else {
         sendDiv.style.display = 'none';
         sendButton.classList.remove('active-button');
     }
+}
+
+function withdrawBitcoin(e) {
+    e.preventDefault();
+    const withdrawButton = document.getElementById('withdraw-button');
+    const withdrawDiv = document.getElementById('withdraw-div');
+    const receiveButton = document.getElementById('receive-button');
+    const receiveDiv = document.getElementById('receive-div');
+    const sendButton = document.getElementById('send-button');
+    const sendDiv = document.getElementById('send-div');
+
+    if(withdrawDiv.style.display === 'none') {
+        withdrawDiv.style.display = 'flex';
+        withdrawButton.classList.add('active-button');
+        receiveDiv.style.display = 'none';
+        receiveButton.classList.remove('active-button');
+        sendDiv.style.display = 'none';
+        sendButton.classList.remove('active-button');
+    } else {
+        withdrawDiv.style.display = 'none';
+        withdrawButton.classList.remove('active-button');
+    }
+
 }
